6461fe2aac47863f30972f2e0ef8b4857a00affb
angelvalentinn <altieriangel1@gmail.com>
2024-09-17 19:29:43 -0300

    aÃtoolbar añadida

--- app/src/main/java/com/example/trabajopractico/MainActivity.kt
+++ app/src/main/java/com/example/trabajopractico/MainActivity.kt
@@ -1,18 +1,24 @@
 package com.example.trabajopractico
-
 import android.content.Intent
+import android.media.Image
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
+import androidx.activity.OnBackPressedCallback
+import androidx.appcompat.widget.Toolbar
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
+
 class MainActivity : AppCompatActivity() {
 
     private lateinit var tvRegistrate: TextView
     private lateinit var btnIniciarSesion: Button
+    private lateinit var ivBack: ImageView
+    private lateinit var toolbar: Toolbar
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -33,7 +39,11 @@ class MainActivity : AppCompatActivity() {
     private fun inicializarComponentes() {
         tvRegistrate = findViewById((R.id.tvRegistrate))
         btnIniciarSesion = findViewById((R.id.btnIniciarSesion))
+        ivBack = findViewById((R.id.ivBack))
+        toolbar = findViewById(R.id.toolbar)
+
     }
+
 
     private fun navegarARegistro() {
         tvRegistrate.setOnClickListener {
