06308be2b0f41401e0bb09ca907db5e3ed6f3157
angelvalentinn <altieriangel1@gmail.com>
2024-09-21 22:15:57 -0300

    añado manejo de base de datos para usuarios registrados y validaciones en la funcionalidad

---
+++ app/src/main/java/com/example/trabajopractico/UserDao.kt
@@ -0,0 +1,22 @@
+package com.example.trabajopractico
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Query
+
+@Dao
+interface UserDao {
+
+    @Query("SELECT * FROM user_entity")
+    fun getAll(): List<User>
+
+    @Query("SELECT * FROM user_entity WHERE email = :email")
+    fun getByEmail(email: String): User
+
+    @Insert
+    fun insert(user: User)
+
+    @Query("DELETE FROM user_entity")
+    fun deleteAllUsers()
+}
